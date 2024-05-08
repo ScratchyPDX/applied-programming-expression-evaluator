77558a456e737fac1834fc582db53e55e2753d33
David Ward <ward.david@live.com>
2024-05-07 18:06:42 -0700

    Initial check-in

---
+++ README.md
@@ -0,0 +1,32 @@
+# Overview
+This program reads a math expression from a user input prompt, evaluates it, and prints the result. It is capable of intepreting equations that include addition, subtraction, multiplication, division, and exponents (using the ^ character).
+
+{Describe your purpose for creating this software.}
+
+{Provide a link to your YouTube demonstration.  It should be a one minute demo of the software running and a walkthrough of the code.}
+
+[Software Demo Video](http://youtube.link.goes.here)
+
+# Development Environment
+The purpose of this project is to demonstrate some of the capabilities of C++. Visual Studio 2022 Community Edition (17.9) was used to code, test, and debug this project. The project was written in C++ 14 Standard, which is the default for Visual Studio 2022.
+
+## The project use headers which perform the following functions:
+[**\<iostream\>**](https://cplusplus.com/reference/iostream/) - Header that defines the standard input/output stream objects
+
+[**\<string\>**](https://cplusplus.com/reference/string/) - This header introduces string types, character traits and a set of converting function
+
+[**\<stack\>**](https://cplusplus.com/reference/stack/stack/) - Stacks are a type of container adaptor, specifically designed to operate in a LIFO context (last-in first-out), where elements are inserted and extracted only from one end of the container.
+
+[**\<cmath\>**](https://cplusplus.com/reference/cmath/?kw=cmath) - Declares a set of functions to compute common mathematical operations and transformations
+
+[**\<vector\>**](https://cplusplus.com/reference/vector/vector/)  Vectors are sequence containers representing arrays that can change in size
+
+[**\<sstream\>**](https://cplusplus.com/reference/sstream/) Header providing string stream classes
+
+[**\<conio\>**](https://stackoverflow.com/a/59812090) Deprecated library that contains routines that are specific to the DOS command line
+
+# Useful Websites
+These websites and articles were helpful while developing this project
+* [cplusplus.com](https://cplusplus.com/reference/)
+* [Shunting Yard Algorithm](https://brilliant.org/wiki/shunting-yard-algorithm/)
+* [Reverse Polish Notation](https://www-stone.ch.cam.ac.uk/documentation/rrf/rpn.html)
---
+++ expression-evaluator.cpp
@@ -0,0 +1,119 @@
+#include <iostream> // standard I/O for data input and output
+#include <stack>    // data storage for evaluated values
+#include <sstream>  // needed for string streaming
+#include <vector>   // data storage for expression values
+#include <cmath>    // needed for pow() function
+#include <conio.h>  // Needed for _getch()
+
+using namespace std;
+
+// provides a score to determine which operation has precedence during evaluation
+int getPrecedence(string op) {
+    if (op == "+" || op == "-") { return 1; }
+    if (op == "*" || op == "/") { return 2; }
+    if (op == "^") { return 3; }
+}
+
+// convert expression string to reverse polish notation using Shunting Yard Algorithm
+vector<string> getExpressionTokens(const string& expression) {
+    vector<string> tokens;  // output queue
+    stack<string> operators;   // operator stack
+
+    istringstream iss(expression); // enable expression string to be streamed as separate values
+    string token;
+
+    
+    while (iss >> token) { // stream a value until white space is encountered
+        if (isdigit(token[0])) {
+            tokens.push_back(token); // push operand to output queue
+        }
+        else {
+            // handle operator precedence
+            while (!operators.empty() && getPrecedence(token) <= getPrecedence(operators.top())) {
+                tokens.push_back(operators.top()); // push operator to output queue
+                operators.pop();
+            }
+            operators.push(token);
+        }
+    }
+
+    // remove remaining operators from stack and place in output queue
+    while (!operators.empty()) {
+        tokens.push_back(operators.top());
+        operators.pop();
+    }
+
+    return tokens;
+}
+
+// main expression evaluation function
+float evaluateExpression(const vector<string>& tokens) {
+    stack<float> value;
+
+    // loop thru each token an in queue
+    for (const string& token : tokens) {
+        // is current token is a number thenplace it onto the stack
+        if (isdigit(token[0])) {
+            float num;
+            istringstream(token) >> num;
+            value.push(num);
+        }
+        else {
+            // evaluate current stack numeric expression
+            float n1 = value.top();
+            value.pop();
+            float n2 = value.top(); 
+            value.pop();
+
+            // perform operation based on operator type
+            switch (token[0]) {
+            case '+': 
+                value.push(n2 + n1); 
+                break;
+            case '-': 
+                value.push(n2 - n1); 
+                break;
+            case '*': 
+                value.push(n2 * n1); 
+                break;
+            case '/': 
+                value.push(n2 / n1); 
+                break;
+            case '^': 
+                value.push(pow(n2,n1)); 
+                break;
+            }
+        }
+    }
+    return value.top();
+}
+
+
+int main() {
+    char choice = 'Y';
+    while (toupper(choice) != 'N') {
+        system("cls");
+
+        // get expression string from user
+        string expression = "";
+        cout << "Enter a mathematical expression (example: 4 + 3 * 2 / 1" << endl;
+        cout << "Ensure to separate each number and operator with a blank space" << endl;
+        cout << "What's the equation? ";
+        getline(cin, expression);
+
+        // break expression string into evaluation parts and place into a queue
+        vector<string> tokens = getExpressionTokens(expression);
+
+        // evaluate result of expression and display results
+        double result = evaluateExpression(tokens);
+        cout << "Result: " << result << endl;
+        cout << endl;
+
+        // get input from user whether to evaluate another expression
+        cout << "Do you want to do another (Y/n)? ";
+        choice = _getch();
+        cout << choice << endl;
+    }
+    system("pause");
+    return 0;
+}
